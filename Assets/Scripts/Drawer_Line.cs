9a47c4b724b21094e98eb659381476df000bf992
Pavel Popchikovsky <ppopchikovsky@gmail.com>
2014-09-30 18:51:14 +0400

    Drawer_Line added

---
+++ Assets/Scripts/Drawer_Line.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Drawer_Line : Drawer
+{
+    #region exposed
+
+    public Material LineMaterial;
+    public Color ColorStart = Color.black;
+    public Color ColorEnd = Color.black;
+
+    #endregion
+
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+
+    #region Drawer
+
+    public override void Draw(TreeData treeData)
+    {
+        Clean();
+        foreach(TreeData.Branch branch in treeData.Branches)
+        {
+            DrawBranch(branch);
+        }
+    }
+
+    #endregion
+
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+
+    #region private functions
+
+    private void Clean()
+    {
+        foreach(Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+    }
+
+    private void DrawBranch(TreeData.Branch branch)
+    {
+        List<TreeData.Vertex> vertices = new List<TreeData.Vertex>(branch.Vertices);
+
+        GameObject go = new GameObject("Branch");
+        go.transform.parent = transform;
+
+        LineRenderer lr = go.AddComponent<LineRenderer>();
+        lr.SetVertexCount(vertices.Count);
+        lr.SetWidth(vertices[0].Size, vertices[vertices.Count-1].Size);
+        lr.material = LineMaterial;
+        lr.SetColors(ColorStart, ColorEnd);
+
+        int index = 0;
+        foreach(TreeData.Vertex vertex in vertices)
+        {
+            lr.SetPosition(index, vertex.Pos);
+            index += 1;
+        }
+    }
+
+    #endregion
+}
