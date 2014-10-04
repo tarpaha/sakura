4b3b459ed27cc34df9998bd1c4e9addad4f751f0
Pavel Popchikovsky <ppopchikovsky@gmail.com>
2014-10-04 14:06:42 +0400

    Drawer_Mesh added

---
+++ Assets/Scripts/Drawer_Mesh.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Drawer_Mesh : Drawer
+{
+    #region exposed
+
+    public Material Material;
+
+    #endregion
+
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+
+    #region Drawer
+
+    public override void Draw(TreeData treeData)
+    {
+        Clean();
+        foreach(TreeData.Branch branch in treeData.Branches)
+        {
+            DrawBranch(branch);
+        }
+    }
+
+    #endregion
+
+    ////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////
+
+    #region private functions
+
+    private void Clean()
+    {
+        foreach(Transform t in transform)
+        {
+            Destroy(t.gameObject);
+        }
+    }
+
+    private void DrawBranch(TreeData.Branch branch)
+    {
+        GameObject go = new GameObject("Branch");
+        go.transform.parent = transform;
+
+        MeshRenderer re = go.AddComponent<MeshRenderer>();
+        re.material = Material;
+
+        MeshFilter meshFilter = go.AddComponent<MeshFilter>();
+        meshFilter.mesh = CreateFromVerticesList(new List<TreeData.Vertex>(branch.Vertices));
+    }
+
+    private Mesh CreateFromVerticesList(List<TreeData.Vertex> list)
+    {
+        Vector3[] vertices = new Vector3[list.Count * 2];
+        for(int i = 0; i < list.Count; i++)
+        {
+            vertices[i * 2 + 0] = list[i].Pos + new Vector3(-list[i].Size/2, 0.0f, 0.0f);
+            vertices[i * 2 + 1] = list[i].Pos + new Vector3(+list[i].Size/2, 0.0f, 0.0f);
+        }
+
+        int[] triangles = new int[6 * (list.Count - 1)];
+        for(int i = 0; i < list.Count - 1; i++)
+        {
+            triangles[6 * i + 0] = 2 * i + 0;
+            triangles[6 * i + 1] = 2 * i + 3;
+            triangles[6 * i + 2] = 2 * i + 1;
+
+            triangles[6 * i + 3] = 2 * i + 0;
+            triangles[6 * i + 4] = 2 * i + 2;
+            triangles[6 * i + 5] = 2 * i + 3;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        return mesh;
+    }
+
+    #endregion
+}
