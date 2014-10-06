cf99b4d47d4ef38c49766751286a89269d44af9c
tarpaha <ppopchikovsky@gmail.com>
2014-10-06 16:49:37 +0400

    Grower_Parts was simplified and renamed to Grower_Variations

--- Assets/Scripts/Grower_Variations.cs
+++ Assets/Scripts/Grower_Variations.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Grower_Parts : Grower
+public class Grower_Variations : Grower
 {
     #region exposed
 
@@ -11,11 +11,8 @@ public class Grower_Parts : Grower
     public float L = 2.0f;
     public float L_delta = 0.02f;
 
-    public float MaxAngleDeviationLeft = 19.0f;
-    public float MaxAngleDeviationRight = 18.0f;
-
-    public int PartsCount = 4;
-    public float PartLengthDeviation = 0.1f;
+    public float MaxAngleDeviationLeft = 25.0f;
+    public float MaxAngleDeviationRight = 24.0f;
 
     #endregion
 
@@ -31,9 +28,6 @@ public class Grower_Parts : Grower
             return new TreeData();
         }
 
-        Random.seed = Seed;
-        List<float> lengts = GetPartLengths(L, PartsCount, PartLengthDeviation);
-
         TreeData.Branch branch = new TreeData.Branch();
 
         Vector2 dir = Vector2.up;
@@ -43,18 +37,19 @@ public class Grower_Parts : Grower
 
         float width = 0.05f;
         float dw = -0.01f;
-        
+
         Random.seed = Seed;
         while(l <= L)
         {
             branch.AddVertex(new TreeData.Vertex(pos, dir, width));
 
             float angle = Random.Range(-MaxAngleDeviationRight, MaxAngleDeviationLeft);
-            Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
 
+            Quaternion rot = Quaternion.AngleAxis(angle, Vector3.forward);
             dir = rot * dir;
-            l += L_delta;
-            
+
+            l += L_delta;           
+
             pos += dir * L_delta;
             width += L_delta * dw;
         }
@@ -63,27 +58,6 @@ public class Grower_Parts : Grower
         treeData.AddBranch(branch);
 
         return treeData;
-    }
-
-
-    private static List<float> GetPartLengths(float L, int partsCount, float deviationFactor)
-    {
-        List<float> result = new List<float>();
-
-        float idealPartLength = L / partsCount;
-        float maxDeviationLength = idealPartLength * deviationFactor;
-
-        float remainingLength = L;
-        for(int i = 0; i < partsCount; i++)
-        {
-            float partLength = idealPartLength + Random.Range(-maxDeviationLength, +maxDeviationLength);
-            result.Add(partLength);
-            remainingLength -= partLength;
-        }
-
-        result[partsCount - 1] += remainingLength;
-
-        return result;
     }
 
     #endregion
